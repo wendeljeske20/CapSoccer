be1986e387bbe60c46e49b1c43b98edc77eb39f6
Wendel <wendelblankjeske@gmail.com>
2019-05-15 21:42:35 -0300

    The modifiers are now created in time interval and in random positions.

--- Scripts/Modifiers/BallModifier.gd
+++ Scripts/Modifiers/BallModifier.gd
@@ -3,7 +3,8 @@ extends Area2D
 var ball = null
 export(String) var ballPath = 'res://Scenes/Modifiers'
 
-onready var ballManager = get_node("../BallManager")
+onready var ballManager = get_parent()
+onready var modifierManager = get_node("../../ModifiersManager")
 
 func _ready():
 	ball = load(ballPath).instance()
@@ -16,5 +17,6 @@ func checkCollisions(collidingBodies):
 	for i in collidingBodies.size():
 		# Checking if it's the ball. Pretty ugly way, but works. *shrug*
 		if "goalScored" in collidingBodies[i].get_node(""):
+			modifierManager.hasModifier = false
 			ballManager.change_ball(ball)
 			get_parent().remove_child(self)
