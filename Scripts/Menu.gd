5938c1b93f5a27612b539c290cad3d760e207fde
RafaelFreita <dfr.rafael@gmail.com>
2019-06-29 21:33:04 -0300

    Removed config button, finished credit screen

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -2,19 +2,19 @@ extends Panel
 
 onready var startButton = get_node("StartButton")
 onready var configurationsButton = get_node("ConfigurationsButton")
-#onready var creditsButton = get_node("CreditsButton")
+onready var creditsButton = get_node("CreditsButton")
 
 onready var quitButton = get_node("QuitButton")
 
 func _ready():
-	#OS.window_fullscreen = true
+	OS.window_fullscreen = true
 	
 	startButton.connect("pressed",self,"Play")
 	startButton.grab_focus()
 	configurationsButton.connect("pressed",self,"Configurations")
 	configurationsButton.grab_focus()
-	#creditsButton.connect("pressed",self,"Credits")
-	#creditsButton.grab_focus()
+	creditsButton.connect("pressed",self,"Credits")
+	creditsButton.grab_focus()
 	quitButton.connect("pressed",self,"Quit")
 	quitButton.grab_focus()
 	#Não entendi o que tava acontecendo antes então só dei o focus aqui
